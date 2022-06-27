512465e60c1b692dfc949e6c2c4d30c6957a31ed
Aryan <61741148+beachchicken@users.noreply.github.com>
2022-06-27 22:16:52 +0545

    Create rpc.c

---
+++ rpc.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(){
+	int a,i,num,pick;
+	
+	daddy:
+		system("cls");
+	printf("\t \t \t ** Rock Paper Or Scissor** \n");
+	printf("\n Enter Your Pick(1 for rock, 2 for paper and 3 for scissor) \n : ");
+	scanf("%d", &a);
+	if (a == 1){
+        	printf("\n You Picked Rock");
+		}
+		else if (a == 2){
+			printf("\n You picked Paper");
+			
+		}
+		else if (a == 3){
+			printf("\n You picked scissor");
+		}
+	
+	 for (i = 1; i <= 1; i++)  
+    {  
+    	srand(time(NULL));
+         num = rand() % 3 + 1; 
+         	
+ }
+         
+        if (num == 1){
+        	printf("\n The computer picked rock \n");
+		}
+		else if (num == 2){
+			printf("\n The computer picked paper \n ");
+			
+		}
+		else if (num == 3){
+		
+			printf("\n The computer picked scissor \n ");
+		}
+	
+
+   if (a == 1 && num == 1){
+   	printf("\n DRAW!");
+   }
+   
+   else if (a == 2 && num == 2){
+   	printf("\n DRAW!");
+   
+   }
+   else if(a == 3 && num == 3){
+   	printf("\n DRAW!");
+   
+   }
+   
+    else if(a == 1 && num == 2){
+   	printf("\n You Loose!");
+   
+   }
+   
+   else if(a == 1 && num == 3){
+   	printf("\n You Win!");
+   
+   }
+    else if(a == 2 && num == 1){
+   	printf("\n You Win!");
+   
+   }
+    else if(a == 2 && num == 3){
+   	printf("\n You Loose!");
+   
+   }
+    else if(a == 3 && num == 1){
+   	printf("\n You Loose!");
+   
+   }
+    else if(a == 3 && num == 2){
+   	printf("\n You Win!");
+   
+   }
+   
+   else{
+   	printf("\n Invalid output!");
+   }
+   
+   
+   printf("\n \n \n\ ");
+   printf("Do you want to continue(1/2): ");
+   scanf("%d",&pick);
+   if (pick == 1){
+   	goto daddy;
+   }
+   else
+   {
+   	exit(0);
+   }
+}
